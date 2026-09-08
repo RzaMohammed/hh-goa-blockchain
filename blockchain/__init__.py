63c0f5c92e04597d21f29ce06a800cbe87a38c24
Rza <rzamohammed404@gmail.com>
2026-09-08 20:15:25 +0530

    feat: export RecordNotFoundError and InsufficientFundsError from blockchain package

--- blockchain/__init__.py
+++ blockchain/__init__.py
@@ -1,6 +1,11 @@
 """
 Blockchain integration module for Ethereum Sepolia smart contract interactions.
 """
-from blockchain.blockchain import BlockchainClient, BlockchainError
+from blockchain.blockchain import BlockchainClient, BlockchainError, RecordNotFoundError, InsufficientFundsError
 
-__all__ = ["BlockchainClient", "BlockchainError"]
+__all__ = [
+    "BlockchainClient",
+    "BlockchainError",
+    "RecordNotFoundError",
+    "InsufficientFundsError",
+]
